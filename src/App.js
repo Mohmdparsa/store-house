f889902c819ea3c189a16760f3a109b8cae39469
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2023-10-08 19:38:05 +0330

    feat(Navbar.jsx): create Navbar component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-function App() {
-  return <div className="App"></div>;
+import Navbar from "./Header component/Navbar";
+const App = () =>{
+  return (
+   <div className="App">
+    <Navbar/>
+
+
+  </div>
+
+  )
 }
 
 export default App;
---
+++ src/Header component/Navbar.jsx
@@ -0,0 +1,12 @@
+import Styles from "./Navbar.module.css"
+const Navbar = () => {
+    return (
+      <>
+        <div className={Styles.navbarContainer}>
+          <h1 className={Styles.title}>StoreHouse</h1>
+        
+        </div>
+      </>
+    );
+  };
+  export default Navbar;

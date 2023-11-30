34c65f3fb8301a57d19219199b6b061f17c3ed1c
Mohammad Parsapasand <mohmd.pars4@gmail.com>
2023-11-30 20:20:33 +0330

    feat(ItemsValidation.js): added validation rules for items form

---
+++ src/Validations/ItemsValidation.js
@@ -0,0 +1,10 @@
+import * as Yup from "yup"
+
+ export const itemSchema = Yup.object().shape({
+    fullname : Yup.string().required("name is required"),
+    photo : Yup.string().url("address is not valid").required("photo is required"),
+    model : Yup.string().required("model is required"),
+    desc : Yup.string().required("decription is required") , 
+    cost : Yup.string().required("cost is required"),
+    group : Yup.string().required("choose group is required")
+})
